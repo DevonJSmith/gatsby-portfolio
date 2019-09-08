c6bc38091415889ac928b91a47647e053a5725de
Devon Smith <djamessmith03@gmail.com>
2019-09-08 17:19:13 -0400

    initial commit

---
+++ config.js
@@ -0,0 +1,74 @@
+
+module.exports = {
+  pathPrefix: '',
+  siteUrl: 'https://devonjsmith.github.io',
+  siteTitle: 'Devon Smith',
+  siteDescription: 'Developer | Designer | Tinkerer',
+  author: 'dsmith',
+  postsForArchivePage: 3,
+  defaultLanguage: 'en',
+  disqusScript: 'https://djamessmith03.disqus.com/embed.js',
+  pages: {
+    home: '/',
+    blog: 'blog',
+    about: 'about',
+    tag: 'tag',
+    archive: 'archive'
+  },
+  social: {
+    github: 'https://github.com/DevonJSmith',
+    linkedin: 'https://www.linkedin.com/in/devon-smith-194393137/',
+    rss: '/rss.xml'
+  },
+  tags: {
+    angular: {
+      description: 'Angular is an open source web application platform.'
+    },
+    electron: {
+      description: 'Electron is a framework for building cross-platform desktop applications with web technology.'
+    },
+    javascript: {
+      description: 'JavaScript is an object-oriented programming language used alongside HTML and CSS to give functionality to web pages.'
+    },
+    laravel: {
+      description: 'Laravel is a PHP framework for building web applications following the MVC pattern.'
+    },
+    nodejs: {
+      name: 'Node.js',
+      description: 'Node.js is a tool for executing JavaScript in a variety of environments.'
+    },
+    rxjs: {
+      name: 'RxJS',
+      description: 'RxJS is a library for reactive programming using Observables, to make it easier to compose asynchronous or callback-based code.'
+    },
+    sass: {
+      description: 'Sass is a stable extension to classic CSS.'
+    },
+    typescript: {
+      description: 'TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.'
+    },
+    react: {
+      description: 'React is an open source JavaScript library used for designing user interfaces.'
+    },
+    vuejs: {
+      name: 'Vue.js',
+      description: 'Vue.js is a JavaScript framework for building interactive web applications.'
+    },
+    flutter:{
+      name: 'Flutter',
+      description: 'placeholder'
+    },
+    dart:{
+      name: 'Dart',
+      description: 'placeholder'
+    },
+    steam: {
+      name: 'Steam',
+      description: 'placeholder'
+    },
+    projects: {
+      name: 'Projects',
+      description: 'placeholder'
+    }
+  }
+}
---
+++ content/2019-09-08-steam-achievements-tracker/index.md
@@ -0,0 +1,10 @@
+---
+title: Steam Achievements Tracker
+date: 2019-09-08
+tags: [flutter, dart, steam, projects]
+path: blog/steam-achievements-tracker
+cover: ./steamachievementstracker-preview.png
+excerpt: A simple mobile app made with the Flutter framework and the Steam API
+---
+
+placeholder text
